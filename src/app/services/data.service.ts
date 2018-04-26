8061532b5155b91f27e70421c3834c3892125a16
BrendanKelly94 <brendan.kelly94@gmail.com>
2018-04-25 18:48:18 -0700

    isolated problem, fixed by switching from observable to promise

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -16,6 +16,9 @@ export class DataService {
   constructor(public jsonp:Jsonp, public http:Http) {
     console.log("constructing");
     fetch('/keys')
+    .then(res => {
+      res.json();
+    })
     .then(keys => {
       console.log(keys);
     })
