1d4dbf2260fd7690adbc6fdaadade5afa7566a2b
BrendanKelly94 <brendan.kelly94@gmail.com>
2018-04-25 18:40:53 -0700

    if this works...

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -17,7 +17,7 @@ export class DataService {
     console.log("constructing");
     http.get('/keys')
     .map(res => res.json())
-    .subscribe(keys => {
+    .subscribe((keys) => {
       console.log(keys);
       this.petKey = keys.petKey;
       this.citiesKey = keys.cityKey;
